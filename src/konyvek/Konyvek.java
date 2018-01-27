4cef9ad061b5e5444b104c19bf0eb345d26429b6
Darázsi Márk <t1@192.168.56.1>
2018-01-27 10:46:14 +0100

    Új főprogram.

--- src/konyvek/Konyvek.java
+++ src/konyvek/Konyvek.java
@@ -5,6 +5,8 @@
  */
 package konyvek;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Darázsi Márk
@@ -16,7 +18,32 @@ public class Konyvek {
      */
     public static void main(String[] args) {
         DB ab = new DB();
-        ab.kiir(1965);
+        Scanner bill = new Scanner(System.in, "cp1250");
+        boolean tovabb = true;
+        String szerzo, cim, eredeti;
+        int ev;
+        
+        while (tovabb) {
+            System.out.println("1-listázás 2-új 3-törlés 0-kilépés");
+            System.out.print("Válassz: ");
+            String v = bill.nextLine();
+            switch (v.charAt(0)) {
+                case '1':
+                    System.out.print("Év: ");
+                    ev = bill.nextInt();
+                    bill.nextLine();
+                    ab.kiir(ev);
+                    break;
+                case '2':
+                    
+                    break;
+                case '3':
+                    
+                    break;
+                default:
+                    tovabb = false;
+            }
+        }
     }
     
 }
