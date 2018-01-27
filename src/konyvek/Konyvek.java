8cac83092f253a6b176033ace7c40b49fd35d67c
Darázsi Márk <t1@192.168.56.1>
2018-01-27 10:17:55 +0100

    Alap változat.

---
+++ src/konyvek/Konyvek.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konyvek;
+
+/**
+ *
+ * @author Darázsi Márk
+ */
+public class Konyvek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DB ab = new DB();
+        ab.kiir(1965);
+    }
+    
+}
